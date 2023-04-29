862cd5a4adfe4bfe0206ca7f3b0f4e76ada6a8a4
georou <georou@users.noreply.github.com>
2023-04-30 09:10:15 +1000

    Handle checking if NTP is enabled cleaner

--- sysutils/pfSense-pkg-LCDproc/files/usr/local/pkg/lcdproc_client.php
+++ sysutils/pfSense-pkg-LCDproc/files/usr/local/pkg/lcdproc_client.php
@@ -108,8 +108,7 @@ function lcdproc_get_nut_ups_status() {
 
 // NTP Status
 function lcdproc_get_ntp_status() { // https://github.com/pfsense/pfsense/blob/master/src/usr/local/www/widgets/widgets/ntp_status.widget.php#L36
-	global $config;
-	if ($config['ntpd']['enable'] == 'enabled') {
+	if (config_get_path('ntpd/enable') == 'enabled') {
 		if (isset($config['system']['ipv6allow'])) {
 			$inet_version = "";
 		} else {
